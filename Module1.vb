57297a315fb48f2660d3c5be6b3adbbe328ad490
Saad Mustafa <56062884+saadmustafa271@users.noreply.github.com>
2019-10-21 10:34:00 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim String1, String2 As String
+        Dim charToRemove, CurrentChar As Char
+        Dim Counter As Integer
+
+        String1 = ""
+        String2 = ""
+        charToRemove = ""
+        CurrentChar = ""
+        Counter = 0
+
+        Console.Write("Enter character to be removed: ")
+        charToRemove = Console.ReadLine
+
+        Console.Write("Enter String to be clean: ")
+        String1 = Console.ReadLine
+
+        For Counter = 1 To Len(String1)
+            CurrentChar = Mid(String1, Counter, 1)
+            If CurrentChar <> charToRemove Then
+                String2 = String2 & CurrentChar
+            End If
+        Next
+
+        Console.Write("Final string = " & String2)
+        Console.ReadKey()
+    End Sub
+
+End Module
