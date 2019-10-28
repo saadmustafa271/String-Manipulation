190f4136c43a2faffdfc969c7358c0c458ec39a7
Saad Mustafa <56062884+saadmustafa271@users.noreply.github.com>
2019-10-28 20:05:23 +0500

    Add files via upload

--- Module1.vb
+++ Module1.vb
@@ -1,31 +1,56 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim String1, String2 As String
-        Dim charToRemove, CurrentChar As Char
-        Dim Counter As Integer
+        Dim Str1 As String
+        Dim validBinaryString As Boolean
+        Dim Worth, Count1, Bit, Value, DenaryValue, Count, AscValue As Integer
 
-        String1 = ""
-        String2 = ""
-        charToRemove = ""
-        CurrentChar = ""
-        Counter = 0
+        Str1 = ""
+        Count = 0
+        AscValue = 0
+        validBinaryString = True
+        Worth = 1
+        Count1 = 0
+        Bit = 0
+        Value = 0
+        DenaryValue = 0
 
-        Console.Write("Enter character to be removed: ")
-        charToRemove = Console.ReadLine
+        Console.Write("Please enter a binary string: ")
+        Str1 = Console.ReadLine
 
-        Console.Write("Enter String to be clean: ")
-        String1 = Console.ReadLine
+        If Len(Str1) > 8 Or Len(Str1) < 1 Then
+            validBinaryString = False
+        End If
 
-        For Counter = 1 To Len(String1)
-            CurrentChar = Mid(String1, Counter, 1)
-            If CurrentChar <> charToRemove Then
-                String2 = String2 & CurrentChar
-            End If
-        Next
+        If validBinaryString = True Then
+            For Count = 1 To Len(Str1)
+                AscValue = Asc(Mid(Str1, Count, 1))
+                If AscValue = 48 Or AscValue = 49 Then
+                    validBinaryString = True
+                Else
+                    validBinaryString = False
+                    Exit For
+                End If
+            Next
+        End If
 
-        Console.Write("Final string = " & String2)
+        If validBinaryString = True Then
+            Console.Write("The string is a valid Binary Number.")
+        Else
+            Console.Write("The string entered is either out of the lenght range or contains non-binary numbers. Please re-enter the string.")
+        End If
+
+        If validBinaryString = True Then
+            For Count1 = 1 To Len(Str1)
+                Bit = Val(Mid(Str1, Len(Str1) - Count1 + 1, 1))
+                Value = Bit * Worth
+                DenaryValue = DenaryValue + Value
+                Worth = Worth * 2
+            Next
+            Console.Write(" The denary value is: " & DenaryValue)
+        End If
         Console.ReadKey()
+
     End Sub
 
 End Module
